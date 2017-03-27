65f7c4edb4955721a0c8848abb9933e5748d936e
Fabio Brito <psychopenguin@gmail.com>
2017-03-27 16:40:31 -0300

    added prometheus client metrics

--- exporter.py
+++ exporter.py
@@ -1,24 +1,28 @@
 import os
+import logging
 from flask import Flask
+from prometheus_client import Gauge
+from prometheus_client.exposition import generate_latest
 from apscheduler.schedulers.background import BackgroundScheduler
 import random
 
+logging.basicConfig(level=logging.os.environ.get('LOG_LEVEL', 'INFO'))
 SERVICE_PORT = int(os.environ.get('SERVICE_PORT', 9213))
 app = Flask(__name__)
 scheduler = BackgroundScheduler()
 
+random_metric = Gauge('a_random_metric', 'random metric')
 
 @scheduler.scheduled_job('interval', seconds=30)
 def update_metrics():
-    global latest_metrics
-    latest_metrics = random.choice(range(100))
+    random_metric.set(random.choice(range(100)))
 
 
 @app.route('/metrics')
 def metrics():
-    return str(latest_metrics)
+    return generate_latest()
 
 if __name__ == '__main__':
-    update_metrics()
     scheduler.start()
-    app.run(host="0.0.0.0", port=SERVICE_PORT, threaded=True)
+    update_metrics()
+    app.run(host="0.0.0.0", port=SERVICE_PORT, threaded=True, debug=True)

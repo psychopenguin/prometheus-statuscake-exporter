01e186454beaf09e9f1aa023c717ec9d57631aae
Fabio Brito <psychopenguin@gmail.com>
2017-03-31 16:23:01 -0300

    working version retrieving current status

--- exporter.py
+++ exporter.py
@@ -1,19 +1,16 @@
 import os
 import logging
 from flask import Flask
-from apscheduler.schedulers.background import BackgroundScheduler
 import statuscake
 
 logging.basicConfig(level=logging.os.environ.get('LOG_LEVEL', 'INFO'))
 SERVICE_PORT = int(os.environ.get('SERVICE_PORT', 9213))
 app = Flask(__name__)
-scheduler = BackgroundScheduler()
 
 @app.route('/metrics')
 def metrics():
     return statuscake.get_metrics()
 
 if __name__ == '__main__':
-    scheduler.add_job(statuscake.update_metrics, 'interval', seconds=30)
-    scheduler.start()
+    statuscake.scheduler.start()
     app.run(host="0.0.0.0", port=SERVICE_PORT, threaded=True, debug=True)
--- statuscake.py
+++ statuscake.py
@@ -3,24 +3,33 @@ import requests
 import logging
 from prometheus_client import Gauge
 from prometheus_client.exposition import generate_latest
+from apscheduler.schedulers.background import BackgroundScheduler
 
 logging.basicConfig(level=logging.os.environ.get('LOG_LEVEL', 'INFO'))
 SC_API_KEY = str(os.environ.get('SC_API_KEY'))
 SC_USERNAME = str(os.environ.get('SC_USERNAME'))
 SC_ENDPOINT = 'https://app.statuscake.com/API/'
 AUTH_HEADERS = {'API': SC_API_KEY, 'Username': SC_USERNAME}
+scheduler = BackgroundScheduler()
 
+labels = ['website', 'testid']
 metrics = {
-    'uptime': Gauge('statuscake_uptime', '7 day % uptime for this site', ['website'])
+    'up': Gauge('statuscake_status', 'check if site is up', labels),
+    'uptime': Gauge('statuscake_uptime', '7 day % uptime for this site', labels)
     }
-
 
 def get_tests():
     req = requests.get(f'{SC_ENDPOINT}/Tests/', headers=AUTH_HEADERS)
     tests = req.json()
     for test in tests:
-        metrics['uptime'].labels(website=test['WebsiteName']).set(test['Uptime'])
+        if test['Status'] == 'Up':
+            metrics['up'].labels(test['WebsiteName'], test['TestID']).set(1)
+        else:
+            metrics['up'].labels(test['WebsiteName'], test['TestID']).set(0)
 
+        metrics['uptime'].labels(test['WebsiteName'], test['TestID']).set(test['Uptime'])
+
+@scheduler.scheduled_job('interval', seconds=30)
 def update_metrics():
     get_tests()
 
